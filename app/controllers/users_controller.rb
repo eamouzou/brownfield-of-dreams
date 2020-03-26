5aefd99be90f3786e6ffd59a44b067e71e992eee
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-25 18:57:27 -0600

    show action in users controller that initializes new github search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show; end
+
+  def show
+    render locals: {
+      search_results: GithubSearch.new(@user.github_token)
+    }
+  end
 
   def new
     @user = User.new
