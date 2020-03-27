cfc073be3072af4981aac77ea3af4780758e4a62
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-27 11:02:53 -0600

    Create method to get a user followers passing that specific url

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -8,15 +8,21 @@ class GithubService
     get_json("/user/repos?page=1&per_page=5")
   end
 
+  def get_followers
+    get_json('/user/followers')
+  end
+
 
   private
 
   def get_json(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
+
   end
 
   def conn
+
     Faraday.new(url: 'https://api.github.com') do |f|
       f.headers["Authorization"] = "token #{@github_token}"
       f.adapter Faraday.default_adapter
